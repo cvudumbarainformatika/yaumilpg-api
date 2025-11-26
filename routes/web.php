48368b9f07bf9394de8efaebb774545f6555cfa4
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2025-11-26 19:23:56 +0700

    rev: A

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/autogen', function () {
-    echo 'Selamat Datang di Toko Online Yaumi LPG Backend v2 WOOOOI AAAA!';
+    echo 'Selamat Datang di Toko Online Yaumi LPG Backend v2 WOOOOI AAAA BBB!';
 });
 
 Route::get('/delete-all-indexes', function () {
