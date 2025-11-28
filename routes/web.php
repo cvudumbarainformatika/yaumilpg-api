129fcf6d28863f8508f4f5bf432cf7a5a59600cc
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2025-11-28 23:43:44 +0700

    TRIGGER!!!

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/autogen', function () {
-    echo 'SELAMAT DATANG DI TOKO LPG YAUMI BACKEND!';
+    echo 'SELAMAT DATANG DI TOKO LPG YAUMI BACKEND V1!';
 });
 
 Route::get('/delete-all-indexes', function () {
